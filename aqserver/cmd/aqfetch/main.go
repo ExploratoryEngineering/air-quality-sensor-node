151808ec16bdc54e9f618dbd498eb956a8833c06
Bjorn Borud <borud@telenordigital.com>
2020-02-26 18:05:24 +0100

    First commit.  Tool for listening to data from device.

---
+++ aqserver/cmd/aqfetch/main.go
@@ -0,0 +1,48 @@
+//
+// This utility connects to the server and receives messages for a
+// given device in real-time, printing out the payload on stdout.
+//
+//
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+
+	"github.com/telenordigital/nbiot-go"
+)
+
+var (
+	collectionID = flag.String("collection", "17dh0cf43jg007", "Collection ID")
+	deviceID     = flag.String("device", "17dh0cf43jg6n4", "Device ID")
+)
+
+func main() {
+	flag.Parse()
+
+	client, err := nbiot.New()
+	if err != nil {
+		log.Fatal("Error creating client:", err)
+	}
+
+	stream, err := client.DeviceOutputStream(*collectionID, *deviceID)
+	if err != nil {
+		log.Fatal("Error creating stream: ", err)
+	}
+	defer stream.Close()
+
+	log.Printf("Listening for messages from device '%s' in collection '%s'", *deviceID, *collectionID)
+	for {
+		data, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("Error receiving data: ", err)
+		}
+
+		log.Printf("received payload: %x", data.Payload)
+	}
+
+}
---
+++ aqserver/go.mod
@@ -0,0 +1,5 @@
+module github.com/ExploratoryEngineering/air-quality-sensor-node/aqserver
+
+go 1.13
+
+require github.com/telenordigital/nbiot-go v0.0.0-20200213152547-104e7da6b46b
