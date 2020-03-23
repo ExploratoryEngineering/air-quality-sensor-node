4251d9bb35dbbf57124ea39d239a664339dd5fbf
Bjorn Borud <borud@borud.org>
2020-03-23 20:02:38 +0100

    Convenience script for importing calibration data

---
+++ server/import-cal-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77j ../calibration/afe3/csv/16-000170.csv
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77l ../calibration/afe3/csv/16-000172.csv
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77n ../calibration/afe3/csv/16-000167.csv
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg781 ../calibration/afe3/csv/16-000160.csv
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg783 ../calibration/afe3/csv/16-000164.csv
+bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg785 ../calibration/afe3/csv/16-000171.csv
