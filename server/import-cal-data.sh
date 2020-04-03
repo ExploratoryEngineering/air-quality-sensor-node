8e560718addf53764f8e4ba519f42962b244720e
Bjorn Borud <borud@borud.org>
2020-04-03 18:11:20 +0200

    Updated import script

--- server/import-cal-data.sh
+++ server/import-cal-data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77j ../calibration/afe3/csv/16-000170.csv
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77l ../calibration/afe3/csv/16-000172.csv
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg77n ../calibration/afe3/csv/16-000167.csv
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg781 ../calibration/afe3/csv/16-000160.csv
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg783 ../calibration/afe3/csv/16-000164.csv
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg785 ../calibration/afe3/csv/16-000171.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg77j ../calibration/afe3/csv/16-000170.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg77l ../calibration/afe3/csv/16-000172.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg77n ../calibration/afe3/csv/16-000167.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg781 ../calibration/afe3/csv/16-000160.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg783 ../calibration/afe3/csv/16-000164.csv
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg785 ../calibration/afe3/csv/16-000171.csv
 
-bin/cal import -c 17dh0cf43jg007 -i 17dh0cf43jg6n4 ../calibration/afe3/csv/16-000171.csv
-
+bin/aq import -c 17dh0cf43jg007 -i 17dh0cf43jg6n4 ../calibration/afe3/csv/16-000171.csv
