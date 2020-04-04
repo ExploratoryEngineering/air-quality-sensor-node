db4e5f97105b75bc452364497a84a27da69935f5
Bjorn Borud <borud@borud.org>
2020-04-04 22:14:54 +0200

    Rearranged import a bit

--- server/cmd/aq/import.go
+++ server/cmd/aq/import.go
@@ -2,18 +2,18 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 
-	"github.com/ExploratoryEngineering/air-quality-sensor-node/server/pkg/calparse"
+	"github.com/ExploratoryEngineering/air-quality-sensor-node/server/pkg/model"
 	"github.com/ExploratoryEngineering/air-quality-sensor-node/server/pkg/store/sqlitestore"
-	"github.com/araddon/dateparse"
 )
 
 // ImportCommand defines the command line parameters for import command
 type ImportCommand struct {
-	CollectionID string `short:"c" long:"collection-id" description:"ID of collection" default:"17dh0cf43jg007" value-name:"<id>"`
-	DeviceID     string `short:"i" long:"device-id" description:"ID of device" value-name:"<id>"`
-	ValidFrom    string `short:"f" long:"valid-from" description:"Start of validity period" value-name:"<date>"`
+	CalFetch bool   `short:"f" long:"fetch-cal" description:"Fetch calibration data from network"`
+	CalURL   string `short:"u" long:"cal-url" description:"Distribution URL for calibration data" default:""`
 }
 
 const (
@@ -32,43 +32,51 @@ func init() {
 // Execute runs the import command
 func (a *ImportCommand) Execute(args []string) error {
 	if len(args) < 1 {
-		log.Fatalf("Please provide name of CSV file")
-	}
-	fileName := args[0]
-
-	db, err := sqlitestore.New(options.DBFilename)
-	defer db.Close()
-
-	cal, err := calparse.ReadCalDataFromCSV(fileName)
-	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Please provide name of JSON file(s)")
 	}
 
-	// If no valid from date is provided we will assume the calibration date of the device
-	cal.ValidFrom = cal.AFECalDate
-	if a.ValidFrom != "" {
-		// Go has a comically stupid date parsing API so we
-		// frivolously use a third party library for parsing this date
-		cal.ValidFrom, err = dateparse.ParseStrict(a.ValidFrom)
-		if err != nil {
-			log.Fatalf("Error parsing ValidFrom date: %v", err)
-		}
-	}
-
-	if cal.DeviceID == "" {
-		cal.DeviceID = a.DeviceID
-	}
-
-	if cal.CollectionID == "" {
-		cal.CollectionID = a.CollectionID
-	}
-
-	id, err := db.PutCal(cal)
-	if err != nil {
-		log.Fatalf("Unable to import calibration entry %s into database: %v", fileName, err)
-	}
-
-	log.Printf("Imported %s, entry id is '%d'", fileName, id)
+	a.importFiles(args)
 
 	return nil
+}
+
+func (a *ImportCommand) importFiles(files []string) {
+	db, err := sqlitestore.New(options.DBFilename)
+	if err != nil {
+		log.Fatalf("Unable to open or create database: %v", err)
+	}
+	defer db.Close()
+
+	for _, fileName := range files {
+		data, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			log.Printf("Cannot read %s, skipping: %v", fileName, err)
+			continue
+		}
+
+		var cal model.Cal
+		err = json.Unmarshal(data, &cal)
+		if err != nil {
+			log.Printf("Cannot unmarshal %s, skipping: %v", fileName, err)
+			continue
+		}
+
+		// Do some validation
+		if cal.DeviceID == "" {
+			log.Printf("DeviceID is not set in %s, skipping", fileName)
+			continue
+		}
+
+		// Override CollectionID if parameter is non-empty
+		if options.HordeCollection != "" {
+			cal.CollectionID = options.HordeCollection
+		}
+
+		id, err := db.PutCal(&cal)
+		if err != nil {
+			log.Fatalf("Unable to import calibration entry %s into database: %v", fileName, err)
+		}
+
+		log.Printf("Imported %s, CollectionID='%s', deviceID='%s', ID='%d'", fileName, cal.CollectionID, cal.DeviceID, id)
+	}
 }
